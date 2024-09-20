8907db6aa91c0d33cf449e2ce051cbcc1fadb966
DataNerd2021 <chasepattee12@gmail.com>
2024-09-20 08:49:03 -0600

    create zookeeper and kafka images and add test kafka script

--- test.py
+++ test.py
@@ -1,7 +1,2 @@
 from kafka import KafkaProducer
-from nba_api.live.nba.endpoints import playbyplay
-import polars as pl
-
-producer = KafkaProducer(bootstrap_servers='localhost:1234')
-for _ in range(100):
-    producer.send('foobar', b'some_message_bytes')
+print('Import Successful')
