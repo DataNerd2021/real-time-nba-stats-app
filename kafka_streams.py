6010b4b3e474ad33f015c502b036dbc6ee48cbcf
DataNerd2021 <chasepattee12@gmail.com>
2024-09-27 15:23:09 -0600

    resolve import error in kafka_streams

--- kafka_streams.py
+++ kafka_streams.py
@@ -1,12 +1,18 @@
 from confluent_kafka import Consumer, Producer
+from nba_api.live.nba.endpoints import playbyplay, boxscore
 
 producer = Producer({'bootstrap.servers': 'localhost:9092'})
 
-def delivery_report(err, msg):
+def delivery_report(err, msg:str):
+    """
+    Reports the success or failure of message delivery.
+    """
     if err is not None:
         print('Message delivery failed: {}'.format(err))
     else:
         print('Game Play Ingested! Topic: {} Partition: {} Offset: {} Preview: {}'.format(msg.topic(), msg.partition(), msg.offset(), msg.value()[:10]))
+
+
 
 producer.produce('test', key='hello', value='world', callback=delivery_report)
 producer.flush()
