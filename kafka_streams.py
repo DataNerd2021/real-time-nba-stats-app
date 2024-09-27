34788a3f99070d8f2a25cfa927fe27aa79024a18
DataNerd2021 <chasepattee12@gmail.com>
2024-09-27 14:35:45 -0600

    add kafka container to docker compose

---
+++ kafka_streams.py
@@ -0,0 +1,15 @@
+from confluent_kafka import Consumer, Producer
+
+producer = Producer({'bootstrap.servers': 'localhost:9092'})
+
+def game_play_ingestion_message(err, msg:str):
+    """
+    This outputs a message to the console indicating whether the game play event was successfully ingested
+    """
+    if err is None:
+        print(f'Game Play Sent to {msg.topic()}. Preview: {msg.value()[:10]}')
+    else:
+        print('Message delivered to {} [{}]'.format(msg.topic(), msg.partition()))
+
+producer.produce('test', key='hello', value='world', callback=game_play_ingestion_message)
+producer.flush()
