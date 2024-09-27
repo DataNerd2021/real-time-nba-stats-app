1f4396dcc4badc343f70568c2161d614081fcfea
DataNerd2021 <chasepattee12@gmail.com>
2024-09-27 15:18:04 -0600

    format delivery_report function and solve bug in docker compose file

--- kafka_streams.py
+++ kafka_streams.py
@@ -1,36 +1,12 @@
 from confluent_kafka import Consumer, Producer
-import socket
-
-def get_ip():
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    try:
-        # doesn't even have to be reachable
-        s.connect(('10.255.255.255', 1))
-        IP = s.getsockname()[0]
-    except Exception:
-        IP = '127.0.0.1'
-    finally:
-        s.close()
-    return IP
-
-print(f"Current IP: {get_ip()}")
-
-conf = {
-    'bootstrap.servers': 'host.docker.internal:9092',
-    'group.id': 'streamlit-app',
-    'auto.offset.reset': 'earliest'
-}
 
 producer = Producer({'bootstrap.servers': 'localhost:9092'})
 
-def game_play_ingestion_message(err, msg:str):
-    """
-    This outputs a message to the console indicating whether the game play event was successfully ingested
-    """
-    if err is None:
-        print(f'Game Play Sent to {msg.topic()}. Preview: {msg.value()[:10]}')
+def delivery_report(err, msg):
+    if err is not None:
+        print('Message delivery failed: {}'.format(err))
     else:
-        print('Message delivered to {} [{}]'.format(msg.topic(), msg.partition()))
+        print('Game Play Ingested! Topic: {} Partition: {} Offset: {} Preview: {}'.format(msg.topic(), msg.partition(), msg.offset(), msg.value()[:10]))
 
-producer.produce('test', key='hello', value='world', callback=game_play_ingestion_message)
+producer.produce('test', key='hello', value='world', callback=delivery_report)
 producer.flush()
