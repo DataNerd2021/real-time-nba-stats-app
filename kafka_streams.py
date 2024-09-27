5f5f0fa0465a7388bb5314d284f48124f9987821
DataNerd2021 <chasepattee12@gmail.com>
2024-09-27 15:09:30 -0600

    attempt new debug for kafka_streams file

--- kafka_streams.py
+++ kafka_streams.py
@@ -1,4 +1,19 @@
 from confluent_kafka import Consumer, Producer
+import socket
+
+def get_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        # doesn't even have to be reachable
+        s.connect(('10.255.255.255', 1))
+        IP = s.getsockname()[0]
+    except Exception:
+        IP = '127.0.0.1'
+    finally:
+        s.close()
+    return IP
+
+print(f"Current IP: {get_ip()}")
 
 conf = {
     'bootstrap.servers': 'host.docker.internal:9092',
