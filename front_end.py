3f5ec43468218e2dfec6ba9eed7c27b3e71cbd3f
Chase Pattee <chasepattee12@gmail.com>
2024-10-03 08:34:29 -0600

    add icons to streamlit app

--- front_end.py
+++ front_end.py
@@ -2,4 +2,6 @@ import streamlit as st
 import pandas as pd
 from confluent_kafka import Producer, Consumer
 
-st.title(":basketball:Real-Time NBA Stats App")
+st.set_page_config(page_title="Real-Time NBA Stats App", page_icon=":basketball:")
+
+st.title(":basketball: Real-Time NBA Stats App")
